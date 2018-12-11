c43d14aed23a305cb72bfab6f69d66606b0e6214
Chris <chris.m@live.nl>
2018-12-11 01:46:48 +0100

    added custom color support

--- BakkesModLoadoutLib/bmloadout.h
+++ BakkesModLoadoutLib/bmloadout.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <map>
 
-#define CURRENT_LOADOUT_VERSION 1
+#define CURRENT_LOADOUT_VERSION 2
 
 struct Header
 {
@@ -18,11 +18,27 @@ struct Item
 	uint8_t paint_index = 0;
 };
 
+struct RGB
+{
+	uint8_t r = 0;
+	uint8_t g = 0;
+	uint8_t b = 0;
+};
+
+struct OverrideColor
+{
+	bool should_override = false;
+	RGB primary_colors;
+	RGB secondary_colors;
+};
+
 struct Body
 {
 	bool blue_is_orange = true;
 	std::map<uint8_t, Item> blue_loadout;
+	OverrideColor blueColor;
 	std::map<uint8_t, Item> orange_loadout;
+	OverrideColor orangeColor;
 };
 
 struct BMLoadout
